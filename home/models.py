1c38961eb7e45f9a4bdeeb7e765a332275d6c3b1
Haseebkhan-ux <asgharabdulhaseeb1@gmail.com>
2025-08-22 04:17:56 -0700

    study models

--- home/models.py
+++ home/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# crud
+
 class Student(models.Model):
     #id = models.AutoField
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    emial = models.EmailField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
-    image = models.ImageField() 
-    file = models.FileField()
 
 
 
-class Product(models.Model):
-    pass
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+
+
+    def __str__(self) -> str:
+        return self.car_name 
